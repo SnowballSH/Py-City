3adbc1a7e0cf1e0096ce3588d8b246fa3da89d8a
SnowballSH <derek070211@gmail.com>
2020-11-24 23:44:04 -0600

    secret

--- PyCity/PyCity/settings.py
+++ PyCity/PyCity/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from .secret import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$8cn8q-z-so$%bm)z9ki8cr7_ek)f1jto2#3hyw8$1a^l7mrs0'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
