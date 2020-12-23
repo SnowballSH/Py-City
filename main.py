f3365ad9ebfa3a98a62174ed97830e40ad10b531
SnowballSH <derek070211@gmail.com>
2020-12-23 17:03:11 -0600

    mouse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.main_analyze import analyze, display
 
 if __name__ == "__main__":
-    data = analyze("https://github.com/SnowballSH/Py-City")
+    data = analyze("https://github.com/SnowballSH/Snow-lang")
     display(data)
